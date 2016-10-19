a094ae637094e880c295f309cec88c32c3b66bd7
Carlos Galvez <carlosgalvezp@gmail.com>
2016-10-19 19:27:02 +0200

    Copy CUDA and cuDNN packages into the Docker image

---
+++ docker/clean_images.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# remove exited containers:
+docker ps --filter status=dead --filter status=exited -aq | xargs -r docker rm -v
+    
+# remove unused images:
+docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r docker rmi
+
+# remove unused volumes:
+find '/var/lib/docker/volumes/' -mindepth 1 -maxdepth 1 -type d | grep -vFf <(
+  docker ps -aq | xargs docker inspect | jq -r '.[] | .Mounts | .[] | .Name | select(.)'
+) | xargs -r rm -fr
