8b0009e27efb7fdae08fcf68e697f60ee4ed88ee
carlosgalvezp <carlos.galvez@volvocars.com>
2016-11-29 16:54:31 +0100

    Add AlexNet lab files

---
+++ term1/exercises/AlexNet-Lab/download_data_and_weights.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://d17h27t6h515a5.cloudfront.net/topher/2016/October/580a829f_train/train.p
+wget https://d17h27t6h515a5.cloudfront.net/topher/2016/October/580d880c_bvlc-alexnet/bvlc-alexnet.npy
+
