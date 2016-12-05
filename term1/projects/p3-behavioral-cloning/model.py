d7a059a4121e2c03892eef73da9964c20d065198
carlosgalvezp <carlos.galvez@volvocars.com>
2016-12-05 16:17:22 +0100

    Allow multiple log files to be used

--- term1/projects/p3-behavioral-cloning/model.py
+++ term1/projects/p3-behavioral-cloning/model.py
@@ -2,20 +2,17 @@ import os
 import cv2
 import csv
 import time
-import glob
 import argparse
 
 def save_model(args, model):
     raise NotImplemented
 
-def load_dataset_(train_dir):
-    """ Loads the raw training dataset """
-    # Declare outputs
+
+def load_log_file(log_file):
     X_train = []
     y_train = []
 
-    # Open CSV file and get image and steering angle from it
-    with open(os.path.join(train_dir, 'driving_log.csv'), 'rb') as csv_file:
+    with open(log_file, 'rb') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
 
         for row in csv_reader:
@@ -24,11 +21,24 @@ def load_dataset_(train_dir):
 
     return X_train, y_train
 
+def load_dataset(log_files):
+    """ Loads the raw training dataset """
+    # Declare outputs
+    X_train = []
+    y_train = []
 
-def get_training_data(train_dir):
+    for log_file in log_files:
+        X_train_i, y_train_i = load_log_file(log_file)
+        X_train.append(X_train_i)
+        y_train.append(y_train_i)
+
+    return X_train, y_train
+
+
+def get_training_data(log_files):
     """ Loads and preprocesses the training data """
     # Load data data
-    X_train, y_train = load_dataset(train_dir)
+    X_train, y_train = load_dataset(log_files)
 
     # Preprocess input
     X_train = preprocess_input(X_train)
@@ -37,10 +47,11 @@ def get_training_data(train_dir):
     out_data = {'X_train': X_train, 'y_train': y_train}
     return out_data
 
-def build_model(train_dir):
+
+def build_model(log_files):
     """ Builds and trains the network given the input data in train_dir """
     # Get training data
-    data = get_training_data(train_dir)
+    data = get_training_data(log_files)
 
     # Build and train the network
     model = define_model()
@@ -48,17 +59,19 @@ def build_model(train_dir):
 
     return model
 
+
 def parse_input():
     """ Sets up the required input arguments and parses them """
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('train_dir',
-                        help='directory containing the training data')
+    parser.add_argument('log_file(s)', nargs='+', dest='log_files',
+                        help='list of CSV files of log data')
     parser.add_argument('-o, --out_dir', dest='out_dir', metavar='',
                         default=time.strftime("%Y%m%d_%H%M%S"),
                         help='directory where the model is stored')
 
     return parser.parse_args()
+
 
 def main():
     """ Main function """
@@ -66,10 +79,11 @@ def main():
     args = parse_input()
 
     # Build a model
-    model = build_model(args.train_dir)
+    model = build_model(args.log_files)
 
     # Save model
     save_model(args.out_dir, model)
+
 
 if __name__ == '__main__':
     main()
