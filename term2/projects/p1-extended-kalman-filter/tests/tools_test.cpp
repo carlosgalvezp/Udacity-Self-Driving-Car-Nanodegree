b6d986beb70eeb26b479f62161a2e9ed6c2aaecf
carlosgalvezp <carlos.galvez@volvocars.com>
2017-03-07 17:04:31 +0100

    Add extra test case for RMSE

--- term2/projects/p1-extended-kalman-filter/tests/tools_test.cpp
+++ term2/projects/p1-extended-kalman-filter/tests/tools_test.cpp
@@ -30,7 +30,25 @@ TEST(ToolsTest, RMS_NoError)
 
 TEST(ToolsTest, RMS_OneMeasurement)
 {
-    EXPECT_EQ(0U, 0U);
+    std::vector<Eigen::VectorXd> estimates;
+    std::vector<Eigen::VectorXd> ground_truth;
+
+    Eigen::VectorXd estimate(n);
+    estimate << 1.5, 4.3, 9.8, 4.2;
+    estimates.push_back(estimate);
+
+    Eigen::VectorXd true_value(n);
+    true_value<< 4.2, 9.8, 4.3, 1.5;
+    ground_truth.push_back(true_value);
+
+    Eigen::VectorXd rmse = Tools::calculateRMSE(estimates, ground_truth);
+    Eigen::VectorXd rmse_expected(n);
+    rmse_expected << 2.7, 5.5, 5.5, 2.7;
+
+    for (std::size_t i = 0U; i < n; ++i)
+    {
+        EXPECT_NEAR(rmse_expected(i), rmse(i), 1.0E-10);
+    }
 }
 
 TEST(ToolsTest, RMS_MultipleMeasurements)
