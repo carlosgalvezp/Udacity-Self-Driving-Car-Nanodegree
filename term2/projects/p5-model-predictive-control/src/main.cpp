6c28c56c8ac4255794f286eb94e40a7d0da71d95
Carlos Galvez <carlosgalvezp@gmail.com>
2017-05-29 19:19:54 +0200

    Remove mpc_model.h since it was not used

--- term2/projects/p5-model-predictive-control/src/main.cpp
+++ term2/projects/p5-model-predictive-control/src/main.cpp
@@ -163,7 +163,7 @@ int main()
                     //
                     // NOTE: REMEMBER TO SET THIS TO 100 MILLISECONDS BEFORE
                     // SUBMITTING.
-//                    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                    std::this_thread::sleep_for(std::chrono::milliseconds(100));
                     ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
                 }
             }
