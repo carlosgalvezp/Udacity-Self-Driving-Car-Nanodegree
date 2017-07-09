7c3b367a056c0b4ed07237684caeaccbfcaf5d3a
Carlos Galvez <carlosgalvezp@gmail.com>
2017-07-09 12:03:56 +0200

    Implement Gaussian Naive Bayes with standard features
    85.35% test accuracy

--- term3/quizzes/PredictionExercise/classifier.py
+++ term3/quizzes/PredictionExercise/classifier.py
@@ -1,43 +1,55 @@
-
+from sklearn.naive_bayes import GaussianNB
 
 class GNB(object):
+    """ Implements a Gaussian Naive Bayes classifier """
 
-	def __init__(self):
-		self.possible_labels = ['left', 'keep', 'right']
+    def __init__(self):
+        self.possible_labels = ['left', 'keep', 'right']
+        self.clf = GaussianNB()
 
-	def train(self, data, labels):
-		"""
-		Trains the classifier with N data points and labels.
+    def train(self, data, labels):
+        """
+        Trains the classifier with N data points and labels.
 
-		INPUTS
-		data - array of N observations
-		  - Each observation is a tuple with 4 values: s, d, 
-		    s_dot and d_dot.
-		  - Example : [
-			  	[3.5, 0.1, 5.9, -0.02],
-			  	[8.0, -0.3, 3.0, 2.2],
-			  	...
-		  	]
+        INPUTS
+        data - array of N observations
+          - Each observation is a tuple with 4 values: s, d,
+            s_dot and d_dot.
+          - Example : [
+	          	[3.5, 0.1, 5.9, -0.02],
+	          	[8.0, -0.3, 3.0, 2.2],
+	          	...
+          	]
 
-		labels - array of N labels
-		  - Each label is one of "left", "keep", or "right".
-		"""
-		pass
+        labels - array of N labels
+          - Each label is one of "left", "keep", or "right".
+        """
+        # Get relevant features
+        X_train = self._get_features(data)
 
-	def predict(self, observation):
-		"""
-		Once trained, this method is called and expected to return 
-		a predicted behavior for the given observation.
+        # Fit classifier
+        self.clf.fit(X_train, labels)
 
-		INPUTS
+    def predict(self, observation):
+        """
+        Once trained, this method is called and expected to return 
+        a predicted behavior for the given observation.
 
-		observation - a 4 tuple with s, d, s_dot, d_dot.
-		  - Example: [3.5, 0.1, 8.5, -0.2]
+        INPUTS
 
-		OUTPUT
+        observation - a 4 tuple with s, d, s_dot, d_dot.
+          - Example: [3.5, 0.1, 8.5, -0.2]
 
-		A label representing the best guess of the classifier. Can
-		be one of "left", "keep" or "right".
-		"""
-		# TODO - complete this
-		return self.possible_labels[1]
+        OUTPUT
+
+        A label representing the best guess of the classifier. Can
+        be one of "left", "keep" or "right".
+        """
+        # Get relevant features
+        X_test = self._get_features(observation)
+
+        # Predict class
+        return self.clf.predict([X_test])
+
+    def _get_features(self, data):
+        return data
--- term3/quizzes/PredictionExercise/prediction.py
+++ term3/quizzes/PredictionExercise/prediction.py
@@ -4,26 +4,33 @@ from classifier import GNB
 import json
 
 def main():
-	gnb = GNB()
-	with open('train.json', 'rb') as f:
-		j = json.load(f)
-	print j.keys()
-	X = j['states']
-	Y = j['labels']
-	gnb.train(X, Y)
 
-	with open('test.json', 'rb') as f:
-		j = json.load(f)
+    # Load training data
+    with open('train.json', 'r') as f:
+        j = json.load(f)
 
-	X = j['states']
-	Y = j['labels']
-	score = 0
-	for coords, label in zip(X,Y):
-		predicted = gnb.predict(coords)
-		if predicted == label:
-			score += 1
-	fraction_correct = float(score) / len(X)
-	print "You got {} percent correct".format(100 * fraction_correct)
+    X = j['states']
+    Y = j['labels']
+
+    # Train classifier
+    gnb = GNB()
+    gnb.train(X, Y)
+
+    # Open test data
+    with open('test.json', 'r') as f:
+	    j = json.load(f)
+
+    X = j['states']
+    Y = j['labels']
+
+    # Evaluate classifier
+    score = 0
+    for coords, label in zip(X,Y):
+        predicted = gnb.predict(coords)
+        if predicted == label:
+	        score += 1
+    fraction_correct = float(score) / len(X)
+    print("You got {} percent correct".format(100 * fraction_correct))
 
 if __name__ == "__main__":
 	main()
