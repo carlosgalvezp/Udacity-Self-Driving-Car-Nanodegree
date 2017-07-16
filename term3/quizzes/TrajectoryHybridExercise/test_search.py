1ae68be459a131b93f3614366b93a995fd67bd0d
Carlos Galvez <carlosgalvezp@gmail.com>
2017-07-16 20:45:47 +0200

    Implement Manhattan distance as heuristic

--- term3/quizzes/TrajectoryHybridExercise/test_search.py
+++ term3/quizzes/TrajectoryHybridExercise/test_search.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 from hybrid_breadth_first import search, reconstruct_path, show_path
 
 X = 1
@@ -32,21 +30,21 @@ START = (0.0,0.0,0.0)
 GOAL = (len(GRID)-1, len(GRID[0])-1)
 
 def main():
-	print "Finding path through grid:"
-	s = ""
-	for row in GRID :
-		row_s = ""
-		for c in row:
-			if c == 0:
-				row_s += "  "
-			else:
-				row_s += "##"
-		row_s += '\n'
-		s += row_s
-	print s
-	closed, came_from, final = search(GRID, START, GOAL)
-	path = reconstruct_path(came_from, GOAL, START, final)
-	show_path(path, START, GOAL)
+    print("Finding path through grid:")
+    s = ""
+    for row in GRID :
+        row_s = ""
+        for c in row:
+            if c == 0:
+                row_s += "  "
+            else:
+                row_s += "##"
+        row_s += '\n'
+        s += row_s
+    print(s)
+    closed, came_from, final = search(GRID, START, GOAL)
+    path = reconstruct_path(came_from, GOAL, START, final)
+    show_path(path, START, GOAL)
 
 if __name__ == "__main__":
-	main()
+    main()
