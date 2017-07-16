e7ee024f17fe2b0b97f6fd6836a5763825f2102e
Carlos Galvez <carlosgalvezp@gmail.com>
2017-07-16 14:18:05 +0200

    Add files for Quintic Polynomial Solver exercise

---
+++ term3/quizzes/TrajectoryExercise1/evaulate_quintic.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import numpy as np
+from quintic import JMT
+
+TEST_CASES = [
+    ([0, 10, 0], [10, 10, 0], 1),
+    ([0, 10, 0], [20, 15, 20], 2),
+    ([5, 10, 2], [-30, -20, -4], 5),
+]
+
+ANSWERS = [[0.0, 10.0, 0.0, 0.0, -0.0, 0.0],
+ [0.0,
+  10.0,
+  0.0,
+  -1.2096774193548387,
+  0.58467741935483875,
+  0.010080645161290322],
+ [5.0,
+  10.0,
+  1.0,
+  -2.126970954356846,
+  0.25078838174273854,
+  -0.0002788381742738588]]
+
+def close_enough(poly, target_poly, eps=0.01):
+	if type(target_poly) != type(poly):
+		target_poly = list(target_poly)
+	if len(poly) != len(target_poly):
+		print "your solution didn't have the correct number of terms"
+		return False
+	for term, target_term in zip(poly, target_poly):
+		if abs(term-target_term) > eps:
+			print "at least one of your terms differed from target by more than {}".format(eps)
+			return False
+	return True
+ 
+def main():
+	for test_case, answer in zip(TEST_CASES, ANSWERS):
+		start, end, T = test_case
+		jmt = JMT(start, end, T)
+		correct = close_enough(jmt, answer)
+		if not correct:
+			print "try again!"
+			return False
+	print "Nice work!"
+	return True
+
+if __name__ == "__main__":
+	main()
---
+++ term3/quizzes/TrajectoryExercise1/quintic.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+# TODO - complete this function
+def JMT(start, end, T):
+    """
+    Calculate the Jerk Minimizing Trajectory that connects the initial state
+    to the final state in time T.
+
+    INPUTS
+
+    start - the vehicles start location given as a length three array
+        corresponding to initial values of [s, s_dot, s_double_dot]
+
+    end   - the desired end state for vehicle. Like "start" this is a
+        length three array.
+
+    T     - The duration, in seconds, over which this maneuver should occur.
+
+    OUTPUT 
+    an array of length 6, each value corresponding to a coefficent in the polynomial 
+    s(t) = a_0 + a_1 * t + a_2 * t**2 + a_3 * t**3 + a_4 * t**4 + a_5 * t**5
+
+    EXAMPLE
+
+    > JMT( [0, 10, 0], [10, 10, 0], 1)
+    [0.0, 10.0, 0.0, 0.0, 0.0, 0.0]
+    """
+    return [1,2,3,4,5,6]
