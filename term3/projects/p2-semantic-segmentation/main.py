e0425219382a2768c869fec980572634b3b963b4
Carlos Galvez <carlosgalvezp@gmail.com>
2017-08-27 11:35:35 +0200

    Log the loss to display in Tensorboard. Fix graph
    Weird-looking graph when not using names for the layers.

--- term3/projects/p2-semantic-segmentation/main.py
+++ term3/projects/p2-semantic-segmentation/main.py
@@ -5,6 +5,7 @@ from math import ceil
 import helper
 import warnings
 import argparse
+import shutil
 from distutils.version import LooseVersion
 import project_tests as tests
 
@@ -45,14 +46,6 @@ def load_vgg(sess, vgg_path):
 
 #tests.test_load_vgg(load_vgg, tf)
 
-def conv1x1(input_tensor, num_outputs):
-    return tf.layers.conv2d(input_tensor,
-                            filters=num_outputs,
-                            kernel_size=1,
-                            strides=1,
-                            kernel_initializer=tf.truncated_normal_initializer(stddev=0.01),
-                            padding='same')
-
 # From
 # https://github.com/MarvinTeichmann/tensorflow-fcn/blob/master/fcn8_vgg.py#L284
 def get_bilinear_filter(f_shape):
@@ -80,32 +73,49 @@ def get_bilinear_filter(f_shape):
     return var
 get_bilinear_filter.count=0
 
-def upsample(input_tensor, num_classes, upsample_factor):
-    # Get input shape
-    input_shape = tf.shape(input_tensor)
+#def upsample(input_tensor, num_classes, upsample_factor):
+#    # Get input shape
+#    input_shape = tf.shape(input_tensor)
 
-    input_batch_size = input_shape[0]
-    input_height = input_shape[1]
-    input_width = input_shape[2]
-    input_channels = input_tensor.get_shape()[3]
+#    input_batch_size = input_shape[0]
+#    input_height = input_shape[1]
+#    input_width = input_shape[2]
+#    input_channels = input_tensor.get_shape()[3]
 
-    # Create initial kernel weights
-    k_size = upsample_factor * 2
-    k_shape = [k_size, k_size, num_classes, input_channels]
-    weights = get_bilinear_filter(k_shape)
+#    # Create initial kernel weights
+#    k_size = upsample_factor * 2
+#    k_shape = [k_size, k_size, num_classes, input_channels]
+#    weights = get_bilinear_filter(k_shape)
 
-    # Define output shape
-    out_shape = [input_batch_size, input_height*upsample_factor, input_width*upsample_factor, num_classes]
-    out_shape_tensor = tf.stack(out_shape)
+#    # Define output shape
+#    out_shape = [input_batch_size, input_height*upsample_factor, input_width*upsample_factor, num_classes]
+#    out_shape_tensor = tf.stack(out_shape)
 
-    # Strides
-    strides = [1, upsample_factor, upsample_factor, 1]
-    return tf.nn.conv2d_transpose(input_tensor,
-                                  weights,
-                                  out_shape_tensor,
-                                  strides=strides,
-                                  padding='SAME')
+#    # Strides
+#    strides = [1, upsample_factor, upsample_factor, 1]
+#    return tf.nn.conv2d_transpose(input_tensor,
+#                                  weights,
+#                                  output_shape=out_shape_tensor,
+#                                  strides=strides,
+#                                  padding='SAME')
 
+def upsample(input_tensor, num_classes, upsample_factor, name):
+    return tf.layers.conv2d_transpose(input_tensor,
+                                      filters=num_classes,
+                                      kernel_size=2*upsample_factor,
+                                      strides=upsample_factor,
+                                      padding='same',
+                                      name=name,
+                                      kernel_initializer=tf.truncated_normal_initializer(stddev=0.01))
+
+def conv1x1(input_tensor, num_outputs, name):
+    return tf.layers.conv2d(input_tensor,
+                            filters=num_outputs,
+                            kernel_size=1,
+                            strides=1,
+                            padding='same',
+                            name=name,
+                            kernel_initializer=tf.truncated_normal_initializer(stddev=0.01))
 
 def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     """
@@ -119,24 +129,24 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     # Upsample last layer 32 times to get output
     with tf.variable_scope('DecoderVars'):
         # Add 1x1 convolution on top of conv7 and upsample
-        conv7_1x1 = conv1x1(vgg_layer7_out, num_classes)
-        conv7_x2 = upsample(conv7_1x1, num_classes, 2)
+        conv7_1x1 = conv1x1(vgg_layer7_out, num_classes, name='conv1x1_1')
+        conv7_x2 = upsample(conv7_1x1, num_classes, 2, 'upsample_1')
 
         # Add 1x1 on top of pool4 to match num_classes
-        pool4_1x1 = conv1x1(vgg_layer4_out, num_classes)
+        pool4_1x1 = conv1x1(vgg_layer4_out, num_classes, 'conv1x1_2')
 
         # Create conv7_x2 + pool4 and upsample
         conv7x2_plus_pool4 = tf.add(conv7_x2, pool4_1x1)
-        conv7_plus_pool4_x2 = upsample(conv7x2_plus_pool4, num_classes, 2)
+        conv7_plus_pool4_x2 = upsample(conv7x2_plus_pool4, num_classes, 2, 'upsample_2')
 
         # Add 1x1 on top of pool3 to match num_classes
-        conv3_1x1 = conv1x1(vgg_layer3_out, num_classes)
+        conv3_1x1 = conv1x1(vgg_layer3_out, num_classes, 'conv1x1_3')
 
         # Add
         conv7x4_plus_pool4x2_plus_pool3 = tf.add(conv3_1x1, conv7_plus_pool4_x2)
 
         # Final upsampling to get FCN-8
-        output = upsample(conv7x4_plus_pool4x2_plus_pool3, num_classes, 8)
+        output = upsample(conv7x4_plus_pool4x2_plus_pool3, num_classes, 8, 'upsample_3')
 
     return output
 
@@ -155,6 +165,9 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     logits = tf.reshape(nn_last_layer, (-1, num_classes))
     cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=correct_label))
     optimizer = tf.train.AdamOptimizer(learning_rate)
+
+    # Add loss to summary
+    tf.summary.scalar('cross-entropy loss', cross_entropy_loss)
 
     # Optimize only the decode weights
     decoder_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='DecoderVars')
@@ -178,10 +191,26 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param correct_label: TF Placeholder for label images
     :param keep_prob: TF Placeholder for dropout keep probability
     """
+    # Create summary
+    logdir = './logs'
+
+    # Delete old logs
+    if os.path.isdir(logdir):
+        shutil.rmtree(logdir)
+
+    # Create a fresh new log directory
+    os.makedirs(logdir)
+
+    # Create writer and summary merge operation
+    summary_merged = tf.summary.merge_all()
+    writer = tf.summary.FileWriter(logdir, tf.get_default_graph())
+
     # Initialize variables
     sess.run(tf.global_variables_initializer())
+    sess.run(tf.local_variables_initializer())
 
     # Loop through epocs
+    step = 0
     for i_epoch in range(epochs):
         batch_nr = 0
         for batch_x, batch_y in get_batches_fn(batch_size):
@@ -192,19 +221,30 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
                                keep_prob: 0.5}
 
             # Feed it to the network and update the weights
-            _, loss_value = sess.run([train_op, cross_entropy_loss], feed_dict=feed_data_train)
+            out = sess.run([train_op, cross_entropy_loss, summary_merged],
+                            feed_dict=feed_data_train)
+
+            loss_value = out[1]
+            summary_out = out[2]
+
+            writer.add_summary(summary_out, step)
+            step += 1
 
             # Compute current loss for display purposes
-            print('[Epoch {}][Batch {}] Loss: {}'.format(i_epoch+1, batch_nr, loss_value))
+            print('[Epoch {}/{}][Batch {}] Loss: {}'
+                  .format(i_epoch+1, epochs, batch_nr, loss_value))
+
+    # Close summary writter
+    writer.close()
 
 #tests.test_train_nn(train_nn)
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Runs the Semantic Segmentation Project')
 
-    parser.add_argument('-e', '--epochs',        dest='epochs',        default=100)
-    parser.add_argument('-b', '--batch',         dest='batch_size',    default=8)
-    parser.add_argument('-l', '--learning_rate', dest='learning_rate', default=0.0001)
+    parser.add_argument('-e', '--epochs',        dest='epochs',        type=int,   default=100)
+    parser.add_argument('-b', '--batch',         dest='batch_size',    type=int,   default=8)
+    parser.add_argument('-l', '--learning_rate', dest='learning_rate', type=float, default=0.0001)
 
     return parser.parse_args()
 
@@ -218,7 +258,7 @@ def run():
     image_shape = (160, 576)
 
     data_dir = './data'
-    log_dir = './logs'
+
     runs_dir = os.path.join('./runs',
                             'e{}_b{}_l{}'.format(epochs, batch_size,
                             str(learning_rate).replace ('.', '_')))
@@ -258,9 +298,6 @@ def run():
         helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
 
         # OPTIONAL: Apply the trained model to a video
-
-        # Write a summary of the process to inspect in TensorBoard
-        helper.create_summary(sess, log_dir)
 
 if __name__ == '__main__':
     run()
